dc90aa0dfc76f7ce2e89637ea039cd2aeee916b1
VenDiago <alizg@code.edu.az>
2022-06-03 08:42:24 +0400

    Add project files.

---
+++ Tag_helpers_SQL_intro_Connection/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tag_helpers_SQL_intro_Connection.Controllers
+{
+    public class HomeController:Controller
+    {
+      public  IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
